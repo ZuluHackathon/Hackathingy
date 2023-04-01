47c65464916fcc86ac07e8d53f9a0146991f2706
colin <colin.williams91@gmail.com>
2023-04-01 14:39:56 -0500

    bundler ửoking, react render dom node ửoking

---
+++ src/server/index.js
@@ -0,0 +1,43 @@
+/* ⇠ ⇡ ⇢ ⇣ */
+/* ۞ ꙰ べ */
+
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const PORT = 9090;
+const DIST_DIR = path.resolve(__dirname, '..', '..', 'dist');
+
+const serveHome = (req, res) => {
+  res.sendFile(path.resolve(DIST_DIR, 'index.html'), (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+};
+/*
+  べべべべべべべべべべべ
+  べべ middleware べべ
+  べべべべべべべべべべべ
+*/
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(DIST_DIR));
+/*
+  べべべべべべべべべべ
+  べべ endpoints べべ
+  べべべべべべべべべべ
+*/
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(DIST_DIR, 'index.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(DIST_DIR, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on http://localhost:${PORT}`);
+});
+
+export default app;
